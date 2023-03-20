dec1a53ffb7c2d23290895f315839a0805ffbab6
Magnus Kulke <magnuskulke@microsoft.com>
2023-03-20 15:31:02 +0100

    Convert tee claim into flat str,str map, as required by AS
    Signed-off-by: Magnus Kulke <magnuskulke@microsoft.com>

--- src/verifier/sev_snp_vtpm/mod.rs
+++ src/verifier/sev_snp_vtpm/mod.rs
@@ -22,24 +22,51 @@ impl Verifier for SevSnpVtpm {
     async fn evaluate(
         &self,
         nonce: String,
-        _attestation: &Attestation,
+        attestation: &Attestation,
     ) -> Result<TeeEvidenceParsedClaim> {
-        let evidence = serde_json::from_str::<VtpmSnpEvidence>(&_attestation.tee_evidence)
+        let evidence = serde_json::from_str::<VtpmSnpEvidence>(&attestation.tee_evidence)
             .context("Failed to deserialize vTPM SEV-SNP evidence")?;
 
         verify_vtpm_quote(&evidence.hcl_report, &evidence.quote, nonce.as_bytes())?;
         verify_snp_report(&evidence.hcl_report)?;
 
-        let claim = build_claim(&evidence);
+        let claim = parse_tee_evidence(&evidence.hcl_report);
         Ok(claim)
     }
 }
 
-fn build_claim(evidence: &VtpmSnpEvidence) -> TeeEvidenceParsedClaim {
-    let report = evidence.hcl_report.snp_report();
-    let tcb = report.reported_tcb;
-    let claims_map = json!({ "tcb": tcb });
-    claims_map as TeeEvidenceParsedClaim
+fn parse_tee_evidence(hcl_report: &HclReportWithRuntimeData) -> TeeEvidenceParsedClaim {
+    let report = hcl_report.snp_report();
+    let numbers = [
+        ("policy_abi_major", report.policy.abi_major()),
+        ("policy_abi_minor", report.policy.abi_minor()),
+        ("policy_smt_allowed", report.policy.smt_allowed()),
+        ("policy_migrate_ma", report.policy.migrate_ma_allowed()),
+        ("policy_debug_allowed", report.policy.debug_allowed()),
+        (
+            "policy_single_socket",
+            report.policy.single_socket_required(),
+        ),
+        // versioning info
+        (
+            "reported_tcb_bootloader",
+            report.reported_tcb.boot_loader as u64,
+        ),
+        ("reported_tcb_tee", report.reported_tcb.tee as u64),
+        ("reported_tcb_snp", report.reported_tcb.snp as u64),
+        (
+            "reported_tcb_microcode",
+            report.reported_tcb.microcode as u64,
+        ),
+        // platform info
+        ("platform_tsme_enabled", report.plat_info.tsme_enabled()),
+        ("platform_smt_enabled", report.plat_info.smt_enabled()),
+    ];
+
+    let mut string_map: BTreeMap<_, _> = numbers.iter().map(|(k, v)| (*k, v.to_string())).collect();
+    string_map.insert("measurement", base64::encode(report.measurement));
+
+    json!(string_map) as TeeEvidenceParsedClaim
 }
 
 fn verify_snp_report(report: &HclReportWithRuntimeData) -> Result<()> {
